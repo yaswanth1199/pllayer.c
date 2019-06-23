aa478d07814eecdd0975d5b6c9267cb29371cf46
yaswanth1199 <51701963+yaswanth1199@users.noreply.github.com>
2019-06-23 16:17:33 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char a[10];
+    scanf("%s",a);
+    if(a[0]=='s'&&a[1]=='u')
+    {
+        printf("yes");
+        
+    }
+    else
+    {
+        printf("no");
+        
+    }
+}
